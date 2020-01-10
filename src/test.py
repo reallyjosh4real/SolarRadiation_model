c5611bf1fd37cd70d3e610628f43f02ac93575c6
reallyjosh4real <reallyjosh4real@gmail.com>
2020-01-10 03:03:03 -0700

    merger

--- src/test.py
+++ src/test.py
@@ -11,6 +11,8 @@ import seaborn as sns
 import dateutil
 
 
+#reads file in and calls the cleanit() function 
+#returns dictionary with coordinates as keys and an array of measured values as values
 
 def readit():
     path = r'/Users/ramozo_88/Capstone1/data'
@@ -28,6 +30,9 @@ def readit():
     
     return measured_DNI_dict, clearsky_DNI_dict, difference_DNI_dict     
 
+#strips files that have been read in as pandas dataframes
+#for information used to plot graphs
+
 def cleanit(df):
 
     new_df1 = df[['Latitude','Longitude','Time Zone','Elevation','Local Time Zone','Clearsky DHI Units']]
@@ -42,6 +47,8 @@ def cleanit(df):
     difference_DNI = clearsky_DNI - measured_DNI
     return lng, lat, difference_DNI, measured_DNI, clearsky_DNI
 
+#calculate and returns averages of measured values for each coordinate in dictionary and sorts
+
 def get_avgs(dct):
     avg = []
     for key in dct:
@@ -51,8 +58,12 @@ def get_avgs(dct):
     
     return avg
 
+#returns top 40 averages
+
 def get_top_avgs(dct):
     return get_avgs(dct)[:40]
+
+#calculate and returns least difference averages between measured and clearsky values in dictionary
 
 def get_least_diff_avgs(dict):
     avg = []
@@ -63,8 +74,12 @@ def get_least_diff_avgs(dict):
     
     return avg
 
+#returns the 40 that have the least difference between measured and clearsky values
+
 def get_bottom_avgs(dct):
     return get_least_diff_avgs(dct)[:40]
+
+#returns coordinates in list
 
 def get_top_coords(lst):
     return [coord[0] for coord in lst]
@@ -77,7 +92,7 @@ def geoplot_all_points():
     for point in measured_DNI_dict.keys():
         folium.Circle(location=[point[0], point[1]], radius=1200, color='yellow', fill_color='yellow').add_to(denver_map1)
 
-def geoplot_top(lst):
+def geoplot_top_producers(lst):
     denver_map2 = folium.Map(location=[39.75,-104.999338],
                         zoom_start=10,
                         tiles="Cartodbpositron")
@@ -85,7 +100,7 @@ def geoplot_top(lst):
     for point in lst:
         folium.Circle(location=[point[0], point[1]], radius=1200, color='orange', fill_color='orange').add_to(denver_map2)
 
-def geoplot_least(lst):
+def geoplot_least_diff_toclearsky(lst):
     denver_map3 = folium.Map(location=[39.75,-104.999338],
                         zoom_start=10,
                         tiles="Cartodbpositron")
@@ -102,6 +117,6 @@ def color_eval():
 if __name__ == "__main__":
     
     measured_DNI_dict, clearsky_DNI_dict, difference_DNI_dict  = readit()
-    top_producers = geoplot_top(get_top_coords(get_top_avgs(measured_DNI_dict)))
-    least_differentials = geoplot_least(get_top_coords(get_bottom_avgs(difference_DNI_dict)))
+    top_producers = geoplot_top_producers(get_top_coords(get_top_avgs(measured_DNI_dict)))
+    least_differentials = geoplot_least_diff_toclearsky(get_top_coords(get_bottom_avgs(difference_DNI_dict)))
 
